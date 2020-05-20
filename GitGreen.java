b9c0320ebb6455ae8e5014cb3e629f8a1d5bb49f
“yz1837182816” <yangzhaomm@163.com>
2020-05-20 22:41:38 +0800

    "05-20"

--- GitGreen.java
+++ GitGreen.java
@@ -8,14 +8,15 @@ import java.util.concurrent.TimeUnit;
 public class GitGreen {
     public static void main(String[] args) throws IOException, InterruptedException {
         SimpleDateFormat format = new SimpleDateFormat("MM-dd");
-        //for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 30; i++) {
+            System.out.println(i);
             OutputStream fos = new FileOutputStream("hello.txt", true);
             fos.write(97);
             Runtime.getRuntime().exec("git add .").waitFor();
             Runtime.getRuntime().exec("git commit -m \""+ format.format(new Date()) +"\"").waitFor();
             Runtime.getRuntime().exec("git push origin master").waitFor();
-        //TimeUnit.DAYS.sleep(1);
+            TimeUnit.DAYS.sleep(1);
             fos.close();
-        //}
+        }
     }
 }
